0308858f25c4ef9d8f1f25690eebaa83e309761c
Rajat <rajatmittal129@gmail.com>
2021-03-06 19:17:55 +0530

    lec1

---
+++ lec1_js_intro/intro.js
@@ -0,0 +1,23 @@
+// console.log("hello there");
+
+let temp = 100;
+// console.log(temp);
+
+let data = {
+    name : "Rajat",
+    place : 171,
+    
+    movies : {
+        type : "avenger",
+
+    },
+    
+
+    array : [1 , 2 , 3 , 4, "hi i am a value" ]
+
+   
+}
+
+let key = "name";
+
+console.log(data[key]);
