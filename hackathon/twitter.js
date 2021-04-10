d9743c83703e5d84ecb134c9f3c8bba7b56eb9e7
Rajat <rajatmittal129@gmail.com>
2021-04-11 01:13:59 +0530

    twitter activity

---
+++ hackathon/twitter.js
@@ -0,0 +1,110 @@
+const puppeteer = require("puppeteer");
+const id = "kumasanyam122@gmail.com";
+const pass = "122kumasanyam";
+const user = "sanyam50902420";
+
+
+(async function(){
+
+    let browser = await puppeteer.launch({headless : false, defaultViewport: null , args:["--start-maximized"]});
+    let allpages = await browser.pages();
+    let tab = allpages[0];
+
+    //login to twitter
+
+    await tab.goto("https://twitter.com/LOGIN");
+    await tab.waitForSelector('[name="session[username_or_email]"]');
+    await tab.type('[name="session[username_or_email]"]',id);
+    await tab.type('[name="session[password]"]',pass);
+    await tab.click('[data-testid="LoginForm_Login_Button"]'); 
+
+    await tab.waitForSelector('.css-1dbjc4n.r-ymttw5.r-1f1sjgu [name="session[username_or_email]"]');
+    await tab.type('.css-1dbjc4n.r-ymttw5.r-1f1sjgu [name="session[username_or_email]"]',user);
+    await tab.type('.css-1dbjc4n.r-16y2uox.r-1wbh5a2 [name="session[password]"]',pass);
+    await tab.click('[data-testid="LoginForm_Login_Button"]');
+
+    let task = process.argv[2];
+    
+    if(task=="follow"){
+        let person = process.argv[3];
+        follow(tab,person);
+    }else if(task=="tweet"){
+        let text = process.argv.slice(3);
+        tweet(tab,text);
+    }else if(task=="like"){
+        let person = process.argv[3];
+        like(tab,person);
+    }else if(task=="comment"){
+        let person = process.argv[3];
+        let cmmt = process.argv.slice(4);
+        comment(tab,person,cmmt);
+    }
+
+})();
+
+async function tweet(tab,text){
+
+    let content="";
+
+    for(let i=0;i<text.length;i++){
+        content=content+text[i]+" ";
+        console.log(text[i]);
+    }
+
+    // console.log(content);
+
+    await tab.waitForSelector('.css-1dbjc4n.r-xoduu5.r-xyw6el.r-mk0yit.r-13qz1uu');
+    await tab.type('.css-1dbjc4n.r-xoduu5.r-xyw6el.r-mk0yit.r-13qz1uu',content);
+    await tab.waitForSelector('[data-testid="tweetButton"]');
+    await tab.click('[data-testid="tweetButton"]');
+
+}
+
+async function follow(tab,person){
+
+    await tab.waitForSelector('[enterkeyhint="search"]');
+    await tab.type('[enterkeyhint="search"]',person);
+    await tab.waitForSelector('[role="listitem"]');
+    let alltags = await tab.$$('[role="listitem"]');    
+    await alltags[0].click();
+    await tab.waitForSelector('.css-1dbjc4n.r-6gpygo.r-bcqeeo');
+    await tab.click('.css-1dbjc4n.r-6gpygo.r-bcqeeo');
+
+}
+
+async function like(tab,person){
+
+    await tab.waitForSelector('[enterkeyhint="search"]');
+    await tab.type('[enterkeyhint="search"]',person);
+    await tab.waitForSelector('[role="listitem"]');
+    let alltags = await tab.$$('[role="listitem"]');    
+    await alltags[0].click();
+    await tab.waitForTimeout(6000);
+    await tab.waitForSelector('[data-testid="like"]');
+    let alllike = await tab.$$('[data-testid="like"]');
+    await alllike[0].click();
+
+}
+
+async function comment(tab,person,cmmt){
+
+    let content = "";
+    for(let i=0;i<cmmt.length;i++){
+        content = content + cmmt[i]+" ";
+    }
+
+    await tab.waitForSelector('[enterkeyhint="search"]');
+    await tab.type('[enterkeyhint="search"]',person);
+    await tab.waitForSelector('[role="listitem"]');
+    let alltags = await tab.$$('[role="listitem"]');    
+    await alltags[0].click();
+    await tab.waitForTimeout(6000);
+    await tab.waitForSelector('[data-testid="reply"]');
+    let alllike = await tab.$$('[data-testid="reply"]');
+    await alllike[0].click();   
+    await tab.waitForSelector('[data-testid="tweetTextarea_0"]');
+    await tab.type('[data-testid="tweetTextarea_0"]',content);
+
+}
+
+
